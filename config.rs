75b0e77036d590beb834568039bbd3b1469b7640
rilysh <luhamailbox@gmail.com>
2022-12-29 22:34:44 +0530

    Add files via upload

---
+++ config.rs
@@ -0,0 +1,5 @@
+pub static APP_ID: &str = "s";
+pub static VERSION: &str = "rilys-build";
+pub static PKGNAME: &str = "Flash RSS";
+pub static PROFILE: &str = "RSS?";
+pub static LOCALEDIR: &str = ".";
